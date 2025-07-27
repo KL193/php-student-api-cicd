cc7a9f1be62bc3661f7175b0a8990b6db5271252
KL193 <lakshankasthuriarachchi@gmail.com>
2025-07-27 12:38:37 +0530

    Add phone number update functionality for students

---
+++ update_phone.php
@@ -0,0 +1,15 @@
+<?php
+include 'db.php';
+$data = json_decode(file_get_contents("php://input"));
+
+$studentID = $data->StudentID;
+$phone = $data->PhoneNumber;
+
+$sql = "UPDATE student SET PhoneNumber='$phone' WHERE StudentID=$studentID";
+
+if ($conn->query($sql) === TRUE) {
+    echo json_encode(["message" => "Phone updated"]);
+} else {
+    echo json_encode(["message" => "Error updating phone"]);
+}
+?>
